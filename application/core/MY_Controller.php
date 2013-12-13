58404191e4ca4348ebac90f40753a562bfa58a99
Chafik_Daggag <daggag.chafik@gmail.com>
2013-12-13 12:07:11 +0100

    Modification de MY_Controller

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -5,10 +5,35 @@ class MY_Controller extends CI_Controller {
 		parent::__construct();
 	}
 	
-	//On genere l'affichage
+	/**
+	 * Vérification de la présence de donnée sde session
+	 * @return boolean
+	 */
+	private function est_connecte() {
+		if($this->session->userdata('vis_matricule')) 
+			return true;
+		return false;
+	}
+	
+	/**
+	 * Redirection si pas de session
+	 */
+	function verifier_session() {
+		if(!$this->est_connecte())
+			redirect('connexion_c','refresh');
+	}
+	
+	/**
+	 * Gestion de l'affichage
+	 * @param array $data
+	 */
 	function generer_affichage($data) {
+		$data['menu'] = false;
+		if($this->est_connecte())
+			$data['menu'] = true;
 		$this->load->view('templates/mainTemplate_v',$data);
 	}
 }
+
 /* End of file MY_Controller.php */
 /* Location : ./application/core/MY_Controller.php */
