2cfb4ab2747b9d0e62aca65ea56547dbc2cb8eed
Chafik_Daggag <daggag.chafik@gmail.com>
2013-12-17 11:25:24 +0100

    Modification du controller Medicament, des vues medicament, affichage medicament etc ...

--- application/controllers/Medicament_c.php
+++ application/controllers/Medicament_c.php
@@ -10,16 +10,25 @@ class Medicament_c extends MY_Controller{
 	}
 	
 	public function index(){
-		$data['title'] = 'Consulter les médicaments';
-		$data['content'] = 'pages/medicament_v';
-		$this->generer_affichage($data);
+// 		$data['title'] = 'Consulter les médicaments';
+// 		$data['content'] = 'pages/medicament_v';
+// 		$this->generer_affichage($data);
+		$data['afficherMedicament'] = false;
+		$this->generer_affichage_medicament($data);
 	
 	}
 	
 	public function afficher(){
-		
-		
+		$data['afficherPraticien'] = true;
+		$data['medicament'] = $this->Medicament_m->getInfosMedicament($this->input->post('medicament'));
+		$this->generer_affichage_medicament($data);
 	
+	}
+	private function generer_affichage_medicament($data) {
+		$data['medicaments'] = $this->Medicament_m->getListeMedicaments();
+		$data['title'] = 'Medicaments';
+		$data['content'] = 'pages/medicament_v';
+		$this->generer_affichage($data);
 	}
 	
 }
--- application/views/pages/medicament_v.php
+++ application/views/pages/medicament_v.php
@@ -1,3 +1,9 @@
 <div id="bloc_medicament" class="contenu">
 	<h1> Médicaments </h1>
+<?php 
+		$this->load->view('pages/includes/formRechercheMedicament_v').br();
+		
+		if (isset($afficherMedicament) && $afficherMedicament)
+			$this->load->view('pages/includes/affichageMedicament_v');
+	?>
 </div>
